a8e2f2dbdb037ae128933a7c95d5d9357802afd2
tera3939 <tera3939.dev@gmail.com>
2019-01-30 00:26:01 +0900

    設定資料集

---
+++ setting.py
@@ -0,0 +1,14 @@
+import os.path
+from Crypto.PublicKey import RSA
+
+
+USERNAME = "user_name"
+DOMAIN = "domain.com"
+PRIVATEKEY_FILE = "privatekey.pem"
+if os.path.exists(PRIVATEKEY_FILE):
+    with open(PRIVATEKEY_FILE, "r") as f:
+        KEYPAIR = RSA.import_key(f.read())
+else:
+    with open(PRIVATEKEY_FILE, "wb") as f:
+        KEYPAIR = RSA.generate(2048)
+        f.write(KEYPAIR.export_key("PEM"))
