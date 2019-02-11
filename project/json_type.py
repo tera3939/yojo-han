7b59d5521b3e6c381991a70327e2694743729738
tera3939 <tera3939.dev@gmail.com>
2019-02-11 15:34:02 +0900

    JsonTypeを更新

--- project/json_type.py
+++ project/json_type.py
@@ -1,3 +1,9 @@
-from typing import Any, Dict
+import typing as t
 
-Json = Dict[str, Any]
+
+# https://github.com/python/typing/issues/182#issuecomment-186306678
+_JSONType_0 = t.Union[str, int, float, bool, None, t.Dict[str, t.Any], t.List[t.Any]]
+_JSONType_1 = t.Union[str, int, float, bool, None, t.Dict[str, _JSONType_0], t.List[_JSONType_0]]
+_JSONType_2 = t.Union[str, int, float, bool, None, t.Dict[str, _JSONType_1], t.List[_JSONType_1]]
+_JSONType_3 = t.Union[str, int, float, bool, None, t.Dict[str, _JSONType_2], t.List[_JSONType_2]]
+Json = t.Union[str, int, float, bool, None, t.Dict[str, _JSONType_3], t.List[_JSONType_3]]
